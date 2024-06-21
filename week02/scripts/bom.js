0954f41657290ed7e3a69c10685c738ade7da73d
McDonald W De-zeade <mcdonald00dezeade@gmail.com>
2024-06-21 16:55:35 +0100

    made changes

---
+++ week02/scripts/bom.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button = addEventListener('click', () => {
+    if (input.value != '') {
+        console.log('Please enter a string!');
+    }
+});
