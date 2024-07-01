ff7868d758d228600039686062f29592fa1a3436
McDonald W De-zeade <mcdonald00dezeade@gmail.com>
2024-07-01 12:28:46 +0100

    done with home page response image

---
+++ scripts/page-visit.js
@@ -0,0 +1,15 @@
+const visitDisplay = document.querySelector('#visit');
+
+let numVisits = Number(window.localStorage.getItem("numVisits - Is")) || 0;
+
+if (numVisits !== 0) {
+    visitDisplay.textContent = numVisits;
+}
+
+else {
+    visitDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-Is", numVisits);
